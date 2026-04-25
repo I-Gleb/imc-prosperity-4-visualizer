3115e1c57488c170d4ca6a1e81a8429a4ea03247
Gleb Ingman <igr2309@gmail.com>
2026-04-25 09:21:30 +0200

    Updated limits for round 3

--- src/pages/visualizer/utils.ts
+++ src/pages/visualizer/utils.ts
@@ -66,6 +66,18 @@ export function getPositionLimit(algorithm: Algorithm, symbol: ProsperitySymbol)
     EMERALDS: 80,
     ASH_COATED_OSMIUM: 80,
     INTARIAN_PEPPER_ROOT: 80,
+    HYDROGEL_PACK: 200,
+    VELVETFRUIT_EXTRACT: 200,
+    VEV_4000: 300,
+    VEV_4500: 300,
+    VEV_5000: 300,
+    VEV_5100: 300,
+    VEV_5200: 300,
+    VEV_5300: 300,
+    VEV_5400: 300,
+    VEV_5500: 300,
+    VEV_6000: 300,
+    VEV_6500: 300,
   };
 
   if (knownLimits[symbol] !== undefined) {
